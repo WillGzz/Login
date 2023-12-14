005a2067bedd1eb75ca07c22389757664016297a
Will <willygomezr1@gmail.com>
2023-12-14 17:00:20 -0500

    working on login

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,21 @@
+function Login() {
+
+
+
+    
+  return (
+    <div>
+      
+      <h1>Login for a surprise</h1>
+      <button>
+        Login
+      </button> 
+
+      <button>
+        SignUp
+      </button>
+    </div>
+  );
+}
+
+export default Login
